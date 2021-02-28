a643c04c7192aefe982808cdbffdf472752d27bb
matheusBBarni <brehm.matheus@hotmail.com>
2021-02-28 00:20:39 -0300

    First commit

---
+++ src/App.test.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react'
+import App, { calcularNovoSaldo } from './App';
+
+describe('Componente principal', () => {
+  describe('Quando eu abro o app do banco', () => {
+    it('O nome é exibido', () => {
+      render(<App />);
+
+      expect(screen.getByText('ByteBank')).toBeInTheDocument();
+    });
+    it('O saldo é exibido', () => {
+      render(<App />);
+
+      expect(screen.getByText('Saldo:')).toBeInTheDocument();
+    });
+    it('O botão de realizar transação é exibido.', () => {
+      render(<App />);
+      expect(screen.getByText('Realizar operação')).toBeInTheDocument();
+    });
+  });
+  describe('Quando eu realizo uma transação', () => {
+    it('Que é um saque, o valor vai diminuir', () => {
+      const valores = {
+        transacao: 'saque',
+        valor: 50,
+      };
+      const saldo = 150;
+      const novoSaldo = calcularNovoSaldo(valores, saldo);
+      expect(novoSaldo).toBe(100);
+    });
+    it('Que é um deposito, o valor vai aumentar', () => {
+      const valores = {
+        transacao: 'deposito',
+        valor: 50,
+      };
+      const saldo = 150;
+      const novoSaldo = calcularNovoSaldo(valores, saldo);
+      expect(novoSaldo).toBe(200);
+    });
+  });
+});
